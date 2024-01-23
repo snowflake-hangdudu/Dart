6c3cbab7ed722e61dfa850e5d3044abde4f18470
snowflake-hangdudu <15551359775@163.com>
2024-01-23 16:59:22 +0800

    1111

---
+++ hangdudu/bin/hangdudu.dart
@@ -0,0 +1,5 @@
+import 'package:hangdudu/hangdudu.dart' as hangdudu;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hangdudu.calculate()}!');
+}
---
+++ hangdudu/test/hangdudu_test.dart
@@ -0,0 +1,8 @@
+import 'package:hangdudu/hangdudu.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
